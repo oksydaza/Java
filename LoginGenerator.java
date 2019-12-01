38e422ea154faa84507976facd729ffb2d18ba1f
oksydaza <magdawlazlo@gmail.com>
2019-12-01 23:25:02 +0100

    change that function to return new array with lower case names

--- LoginGenerator.java
+++ LoginGenerator.java
@@ -1,5 +1,17 @@
 public class LoginGenerator {
 
+    // change that function to return new array with lower case names
+    public static String[] toLowerCase(String[] studentNames){
+        String[] studentNamesLowerCase = new String[studentNames.length];
+        for (String full_name : studentNames){
+            System.out.println(full_name.toLowerCase());
+        }
+        for (int i = 0; i < studentNames.length; i++){
+            studentNamesLowerCase[i] = studentNames[i].toLowerCase
+        }
+    }
+
+        return studentNamesLowerCase;
     public static void main(String[] args) {
 
         String[] studentNames = { "Budynek Piotr", 
@@ -7,19 +19,12 @@ public class LoginGenerator {
                                   "Wiezowiec Jan",
                                   "Szkieletor Andrzej",
                                   "Domek Marianna" };
+    
+    System.out.println(studentNames);
+    toLowerCase(studentNames);
+
     }
-    //public String toLowerCase(studentNames);
- 
 }
-
-
-
-
-
-
-
-
-
 
 
 

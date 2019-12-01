bc96896760a8818cc120e65701f837ef2781eaf4
oksydaza <magdawlazlo@gmail.com>
2019-12-01 22:13:56 +0100

    adding the main structure

---
+++ LoginGenerator.java
@@ -0,0 +1,29 @@
+public class LoginGenerator {
+
+    public static void main(String[] args) {
+
+        String[] studentNames = { "Budynek Piotr", 
+                                  "Chalupa Krystyna", 
+                                  "Wiezowiec Jan",
+                                  "Szkieletor Andrzej",
+                                  "Domek Marianna" };
+    }
+    //public String toLowerCase(studentNames);
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//public String toLowerCase()
+//split(String regex, int limit)
+
+
